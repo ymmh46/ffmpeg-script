bf01e40c89f13a6d96d6c05aaed27b2761de4cee
ymmh46 <morefun.wp@gmail.com>
2023-05-09 22:15:52 +0800

    add auto-incriment for output file

--- mac-mp4-to-m4a.sh
+++ mac-mp4-to-m4a.sh
@@ -1,4 +1,8 @@
 mkdir output
+i=0
 for f in *.mp4; do
-	ffmpeg -i "$f" -c copy "output/${f%mp4}m4a"
+	((i = i + 1))
+	ffmpeg -i "$f" -c copy "output/${i}_${f%mp4}m4a"
 done
+
+echo "Success converted ${i} mp4 files to m4a."
